9df39579c2f3cbed1a782fd4cd1098b39becf2b6
zluqman206 <zluqman@uw.edu>
2025-04-27 14:45:51 -0700

    Created "retrieve book by ISBN" route

--- src/routes/closed/index.ts
+++ src/routes/closed/index.ts
@@ -3,11 +3,14 @@ import express, { Router } from 'express';
 import { checkToken } from '../../core/middleware';
 import { tokenTestRouter } from './tokenTest';
 import { messageRouter } from './closed_message';
+import { getBookByISBNRouter } from './get_book_by_ISBN'
 
 const closedRoutes: Router = express.Router();
 
 closedRoutes.use('/jwt_test', checkToken, tokenTestRouter);
 
 closedRoutes.use('/c/message', checkToken, messageRouter);
+closedRoutes.use('/c/get_book_by_ISBN', checkToken, getBookByISBNRouter);
+
 
 export { closedRoutes };
