49d5d3bdf5e68e3b19ecddae7089d464c6c2137a
Riley Mansfield <rileymansfield73@gmail.com>
2025-04-27 23:54:08 -0700

    Adding route get book by author

--- src/routes/closed/index.ts
+++ src/routes/closed/index.ts
@@ -4,6 +4,8 @@ import { checkToken } from '../../core/middleware';
 import { tokenTestRouter } from './tokenTest';
 import { messageRouter } from './closed_message';
 import { getBookByISBNRouter } from './get_book_by_ISBN'
+import { getBookByAuthorRouter } from './get_book_by_author';
+
 import { bookRouter } from './book';
 
 const closedRoutes: Router = express.Router();
@@ -12,6 +14,7 @@ closedRoutes.use('/jwt_test', checkToken, tokenTestRouter);
 
 closedRoutes.use('/c/message', checkToken, messageRouter);
 closedRoutes.use('/c/get_book_by_ISBN', checkToken, getBookByISBNRouter);
+closedRoutes.use('/c/get_book_by_author', checkToken, getBookByAuthorRouter);
 closedRoutes.use('/c/book', checkToken, bookRouter);
 
 export { closedRoutes };
