0a4f212a5a3ce074b4623936ea0155d888f628c9
Anthony Chapkin <anthonyvchapkin@gmail.com>
2025-05-09 16:00:37 -0700

    Create query for get books with filter

--- src/routes/closed/get-books-with-filter.ts
+++ src/routes/closed/get-books-with-filter.ts
@@ -9,6 +9,51 @@ const isStringProvided = validationFunctions.isStringProvided;
 getBooksWithFilterRouter.get(
     '/',
     async (request: Request, response: Response) => {
+        // Validation for filter parameters.
+        const author: string = isStringProvided(request.query.author)
+            ? (request.query.author as string)
+            : ''; // Empty string by default
+
+        const title: string = isStringProvided(request.query.title)
+            ? (request.query.title as string)
+            : ''; // Empty string by default
+
+        const minYear: number =
+            isNumberProvided(request.query.minYear) &&
+            +request.query.minYear >= 0
+                ? +request.query.minYear
+                : 0; // Default min year is 0
+
+        const maxYear: number =
+            isNumberProvided(request.query.maxYear) &&
+            +request.query.maxYear >= 0
+                ? +request.query.maxYear
+                : new Date().getFullYear(); // Default max year is current date
+
+        const minRating: number =
+            isNumberProvided(request.query.minRating) &&
+            +request.query.minRating >= 0
+                ? +request.query.minRating
+                : 0; // Default min avg rating is 0
+
+        const maxRating: number =
+            isNumberProvided(request.query.maxRating) &&
+            +request.query.maxRating >= 0
+                ? +request.query.maxRating
+                : 5; // Default max avg rating is 5
+
+        const minRatingCount: number =
+            isNumberProvided(request.query.minRatingCount) &&
+            +request.query.minRatingCount >= 0
+                ? +request.query.minRatingCount
+                : 0; // Default min count of ratings is 0
+
+        const maxRatingCount: number =
+            isNumberProvided(request.query.maxRatingCount) &&
+            +request.query.minRatingCount >= 0
+                ? +request.query.maxRatingCount
+                : Number.POSITIVE_INFINITY; // Default max count of ratings is infinity
+
         // Validation for offset and limit stolen from closed_message.ts.
         const limit: number =
             isNumberProvided(request.query.limit) && +request.query.limit > 0
@@ -26,30 +71,107 @@ getBooksWithFilterRouter.get(
                 ? false
                 : true; // Default is true
 
-        // Validation for filter parameters.
-        const author: string = isStringProvided(request.query.author)
-            ? (request.query.author as string)
-            : ''; // Empty string by default
+        const query = `
+					SELECT 
+                    b.isbn13,
+                    ARRAY_AGG(a.author) AS authors,
+                    b.original_publication_year AS publication,
+                    b.original_title,
+                    b.title,
+                    JSON_BUILD_OBJECT(
+                    'average', ROUND(
+                    (br.ratings_1 * 1.0 + br.ratings_2 * 2.0 + br.ratings_3 * 3.0 + br.ratings_4 * 4.0 + br.ratings_5 * 5.0) /
+                    NULLIF((br.ratings_1 + br.ratings_2 + br.ratings_3 + br.ratings_4 + br.ratings_5), 0), 2
+                    ),
+                    'count', (br.ratings_1 + br.ratings_2 + br.ratings_3 + br.ratings_4 + br.ratings_5),
+                    'rating_1', br.ratings_1,
+                    'rating_2', br.ratings_2,
+                    'rating_3', br.ratings_3,
+                    'rating_4', br.ratings_4,
+                    'rating_5', br.ratings_5
+                    ) AS ratings,
+                    JSON_BUILD_OBJECT(
+                    'large', b.image_url,
+                    'small', b.small_image_url
+                    ) AS icons
+                    FROM 
+                    books b 
+                    JOIN author_books ab ON b.isbn13 = ab.isbn13
+                    JOIN authors a ON a.author_id = ab.author_id
+                    JOIN book_ratings br ON br.isbn13 = ab.isbn13
+					WHERE a.author ILIKE '%' || $1 || '%'
+					AND b.title ILIKE '%' || $2 || '%'
+					AND b.original_publication_year BETWEEN $3 AND $4
+					AND
+					ROUND(
+                    (br.ratings_1 * 1.0 + br.ratings_2 * 2.0 + br.ratings_3 * 3.0 + br.ratings_4 * 4.0 + br.ratings_5 * 5.0) /
+                    NULLIF((br.ratings_1 + br.ratings_2 + br.ratings_3 + br.ratings_4 + br.ratings_5), 0), 2
+                    ) BETWEEN $5 AND $6
+					AND
+					(br.ratings_1 + br.ratings_2 + br.ratings_3 + br.ratings_4 + br.ratings_5)
+					BETWEEN $7 AND $8::numeric
+                    GROUP BY 
+                    b.isbn13,
+                    b.original_publication_year,
+                    b.original_title,
+                    b.title,
+                    br.ratings_1,
+                    br.ratings_2,
+                    br.ratings_3,
+                    br.ratings_4,
+                    br.ratings_5,
+                    b.image_url,
+                    b.small_image_url
+                    ORDER BY 
+                    b.isbn13
+                    LIMIT 
+                    $9
+                    OFFSET 
+                    $10;
+                    `;
 
-        const title: string = isStringProvided(request.query.title)
-            ? (request.query.title as string)
-            : ''; // Empty string by default
+        try {
+            const paramValues = [
+                author,
+                title,
+                minYear,
+                maxYear,
+                minRating,
+                maxRating,
+                minRatingCount,
+                maxRatingCount === Number.POSITIVE_INFINITY
+                    ? 'infinity'
+                    : maxRatingCount,
+                limit,
+                offset,
+            ];
+            const { rows } = await pool.query(query, paramValues);
 
-        const minYear: number = isNumberProvided(request.query.minYear)
-            ? +request.query.minYear
-            : 0; // Default min year is 0
+            let count = null;
+            if (getTotal) {
+                const result = await pool.query(
+                    'SELECT count(*) AS exact_count FROM books;'
+                );
+                count = result.rows[0].exact_count;
+            }
 
-        const maxYear: number = isNumberProvided(request.query.maxYear)
-            ? +request.query.maxYear
-            : new Date().getFullYear(); // Default max year is current date
-
-        const minRating: number = isNumberProvided(request.query.minRating)
-            ? +request.query.minRating
-            : 0; // Default min avg rating is 0
-
-        const maxRating: number = isNumberProvided(request.query.maxRating)
-            ? +request.query.maxRating
-            : 5; // Default max avg rating is 5
+            response.status(200).send({
+                pagination: {
+                    totalRecords: count,
+                    limit,
+                    offset,
+                    nextPage: limit + offset,
+                    hasMore: getTotal ? offset + limit < count : null,
+                },
+                books: rows,
+            });
+        } catch (error) {
+            console.error('Database error on get books with filter');
+            console.error(error);
+            response
+                .status(500)
+                .send({ message: 'server error - contact support' });
+        }
     }
 );
 
