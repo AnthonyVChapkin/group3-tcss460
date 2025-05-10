8dbb07c80faab7e8fd3cd41513da8c9732a6e79d
Anthony Chapkin <anthonyvchapkin@gmail.com>
2025-05-10 16:49:51 -0700

    Create password validation function

--- src/core/utilities/validationUtils.ts
+++ src/core/utilities/validationUtils.ts
@@ -42,6 +42,55 @@ function isNumber(x: any): x is number {
     return typeof x === 'number';
 }
 
+/**
+ * Validates a password based on predefined rules.
+ *
+ * @param {string} password The password string to validate.
+ * @returns {string[]} An array of error messages if any validation rules are violated, otherwise empty.
+ *
+ * Rules:
+ * - At least 12 characters long.
+ * - Must contain at least one uppercase letter (A-Z).
+ * - Must contain at least one lowercase letter (a-z).
+ * - Must contain at least one digit (0-9).
+ * - Must contain at least one special character (!@#$%^&*(),.?":{}|<>).
+ */
+function validatePassword(password: string): string[] {
+    const errors: string[] = [];
+
+    // Password must be at least 12 chars long.
+    if (password.length < 12) {
+        errors.push('Password must be at least 12 characters long.');
+    }
+
+    const uppercaseRegex = /[A-Z]/;
+    const lowercaseRegex = /[a-z]/;
+    const digitRegex = /[0-9]/;
+    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+
+    // Contains at last one uppercase char.
+    if (!uppercaseRegex.test(password)) {
+        errors.push('Password must include at least one uppercase letter.');
+    }
+
+    // Contains at least one lowercase char.
+    if (!lowercaseRegex.test(password)) {
+        errors.push('Password must include at least one lowercase letter.');
+    }
+
+    // Contains at least one number/digit.
+    if (!digitRegex.test(password)) {
+        errors.push('Password must include at least one digit.');
+    }
+
+    // Contains at least one special char.
+    if (!specialCharRegex.test(password)) {
+        errors.push('Password must include at least one special character.');
+    }
+
+    return errors;
+}
+
 // Feel free to add your own validations functions!
 // for example: isNumericProvided, isValidPassword, isValidEmail, etc
 // don't forget to export any
@@ -49,6 +98,7 @@ function isNumber(x: any): x is number {
 const validationFunctions = {
     isStringProvided,
     isNumberProvided,
+    validatePassword,
 };
 
 export { validationFunctions };
